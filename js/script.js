3c8bb1689e8dd4544567cce18b8d4ba62f3721eb
Angela Tsurumaki <angelamutsumi@gmail.com>
2020-08-02 11:24:30 -0300

    Functions handlePodcastsFrom e renderPodcast

--- js/script.js
+++ js/script.js
@@ -16,5 +16,30 @@ function handleRangeChange(event) {
 	var currentFrequency = event.target.value;
 	inputFrequency.value = currentFrequency;
 
-	renderPodcastFrom(currentFrequency);
+	handlePodcastsFrom(currentFrequency);
+}
+
+function handlePodcastsFrom(frequency) {
+	var foundPodcast = null;
+
+	for (var i = 0; i < realPodcasts.length; i++) {
+		var currentPodcast = realPodcasts[i];
+
+		if (currentPodcast.id === frequency) {
+			foundPodcast = currentPodcast;
+			break;
+		}
+	}
+	if (foundPodcast) {
+		renderPodcast(foundPodcast);
+	} else {
+		divPodcasts.innerHTML = 'Nenhum podcast encontrado.';
+	}
+}
+
+function renderPodcast(podcast) {
+	const img = document.createElement('img');
+	img.src = '../img/' + podcast.img;
+
+	divPodcasts.appendChild(img);
 }
