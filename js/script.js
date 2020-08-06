ebc2f639f54ac8254a6accf3edae22198f872a59
Angela Tsurumaki <angelamutsumi@gmail.com>
2020-08-06 10:31:46 -0300

    Array method find, template literals e link Materialize

--- js/script.js
+++ js/script.js
@@ -10,6 +10,7 @@ function start() {
 	divPodcasts = document.querySelector('#divPodcasts');
 
 	inputRange.addEventListener('input', handleRangeChange);
+	handlePodcastsFrom(inputRange.value);
 }
 
 function handleRangeChange(event) {
@@ -22,24 +23,44 @@ function handleRangeChange(event) {
 function handlePodcastsFrom(frequency) {
 	var foundPodcast = null;
 
-	for (var i = 0; i < realPodcasts.length; i++) {
-		var currentPodcast = realPodcasts[i];
+	// for (var i = 0; i < realPodcasts.length; i++) {
+	// 	var currentPodcast = realPodcasts[i];
 
-		if (currentPodcast.id === frequency) {
-			foundPodcast = currentPodcast;
-			break;
-		}
-	}
-	if (foundPodcast) {
-		renderPodcast(foundPodcast);
-	} else {
-		divPodcasts.innerHTML = 'Nenhum podcast encontrado.';
-	}
+	// 	if (currentPodcast.id === frequency) {
+	// 		foundPodcast = currentPodcast;
+	// 		break;
+	// 	}
+	// }
+
+	foundPodcast = realPodcasts.find(function(currentPodcast) {
+		return currentPodcast.id === frequency;
+	});
+	renderPodcast(foundPodcast);
 }
 
 function renderPodcast(podcast) {
-	const img = document.createElement('img');
-	img.src = '../img/' + podcast.img;
+	if (!podcast) {
+		divPodcasts.innerHTML = 'Nenhum podcast encontrado!';
+		return;
+	}
 
-	divPodcasts.appendChild(img);
+	// const img = document.createElement('img');
+	// img.src = '../img/' + podcast.img;
+
+	// const title = document.createElement('h2');
+	// title.textContent = podcast.title;
+
+	// const description = document.createElement('p');
+	// description.textContent = podcast.description;
+
+	// divPodcasts.innerHTML = '';
+	// divPodcasts.appendChild(img);
+	// divPodcasts.appendChild(title);
+	// divPodcasts.appendChild(description);
+
+	divPodcasts.innerHTML = `
+	<img src='../img/${podcast.img}' />
+	<h2> ${podcast.title} </h2>
+	<p> ${podcast.description} </p>
+	`;
 }
